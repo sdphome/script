20ffd37f0b44140687db8efbf0a092b3b7e18a46
Depeng Shao <sdphome@live.cn>
2016-09-02 10:17:49 +0800

    update get_android_log.bat
    Signed-off-by: Depeng Shao <sdphome@live.cn>

--- tools/get_android_log.bat
+++ tools/get_android_log.bat
@@ -11,7 +11,7 @@ for /f "tokens=1,2 delims=/" %%i in ("%Today%") do (
 
 SET date=%mm%-%dd%
 
-set tmp_time=%time:~-11,2%%time:~-8,2%%time:~-5,2%
+set tmp_time=%time:~-11,2%-%time:~-8,2%-%time:~-5,2%
 set tmp_time=%tmp_time: =%
 set d=%date%_%tmp_time%
 mkdir "%d%"
@@ -24,15 +24,16 @@ adb remount
 adb wait-for-devices
 adb logcat -c
 adb shell sync
-mkdir "%d%"
 adb root
 pause
+
+mkdir "%d%"
 adb logcat -c
+start cmd /c "adb shell dmesg > %d%\dmesg.txt"
 start cmd /c "adb logcat -v threadtime > %d%\logcat.txt"
 start cmd /c "adb logcat -b radio -v threadtime > %d%\radiologs.txt"
 start cmd /c "adb logcat -b main -v threadtime > %d%\mainlogs.txt"
 start cmd /c "adb logcat -b system -v threadtime > %d%\systemlogs.txt"
 start cmd /c "adb logcat -b events -v threadtime > %d%\eventlogs.txt"
 start cmd /c "adb shell cat /proc/kmsg > %d%\kernel.txt"
-start cmd /c "adb shell dmesg > %d%\dmesg.txt"
 start cmd /c "adb shell top -t > %d%\threads.txt"
